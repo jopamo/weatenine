c77314da71df9aaa15db54ebd495f4d5e6002ef3
jopamo <p@1g4.org>
2023-10-28 00:07:38 -0400

    make that 504 random squares

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const colors = [
-    { r: 255, g: 0, b: 0 },
-    { r: 0, g: 255, b: 0 },
-    { r: 0, g: 0, b: 255 },
-  ];
+  // 500 random colors: red, green, or blue
+  const colors = Array.from({ length: 504 }, () => {
+    const randomNum = Math.floor(Math.random() * 3);
+    switch (randomNum) {
+      case 0:
+        return { r: 255, g: 0, b: 0 }; // Red
+      case 1:
+        return { r: 0, g: 255, b: 0 }; // Green
+      default:
+        return { r: 0, g: 0, b: 255 }; // Blue
+    }
+  });
 
   return (
     <div className="App">
