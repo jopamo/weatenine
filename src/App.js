079bf99b81e79e4c156335927ff52c70b83e8854
jopamo <p@1g4.org>
2023-10-28 00:22:17 -0400

    just fill the screen, it's more fun this way

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  // 500 random colors: red, green, or blue
-  const colors = Array.from({ length: 504 }, () => {
-    const randomNum = Math.floor(Math.random() * 3);
-    switch (randomNum) {
-      case 0:
-        return { r: 255, g: 0, b: 0 }; // Red
-      case 1:
-        return { r: 0, g: 255, b: 0 }; // Green
-      default:
-        return { r: 0, g: 0, b: 255 }; // Blue
-    }
-  });
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    const squareSize = 25; // size of each square in pixels
+    const numHorizontalSquares = Math.floor(window.innerWidth / squareSize);
+    const numVerticalSquares = Math.floor(window.innerHeight / squareSize);
+    const totalSquares = numHorizontalSquares * numVerticalSquares;
+
+    const generatedColors = Array.from({ length: totalSquares }, () => {
+      const randomNum = Math.floor(Math.random() * 3);
+      switch (randomNum) {
+        case 0:
+          return { r: 255, g: 0, b: 0 }; // Red
+        case 1:
+          return { r: 0, g: 255, b: 0 }; // Green
+        default:
+          return { r: 0, g: 0, b: 255 }; // Blue
+      }
+    });
+
+    setColors(generatedColors);
+  }, []);
 
   return (
     <div className="App">
